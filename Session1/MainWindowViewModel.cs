9c7af3c3f53457c5636e96122ff2000ee836d2e1
Nima <nima.ghomri@gmail.com>
2024-09-17 17:34:39 +0330

    Nested binding

--- Session1/MainWindowViewModel.cs
+++ Session1/MainWindowViewModel.cs
@@ -4,16 +4,6 @@ namespace Session1;
 
 public partial class MainWindowViewModel : ObservableObject
 {
-    [ObservableProperty, NotifyPropertyChangedFor(nameof(FullName))]
-    string firstName;
-
-    partial void OnFirstNameChanged(string? oldValue, string newValue)
-    {
-
-    }
-
-    [ObservableProperty, NotifyPropertyChangedFor(nameof(FullName))]
-    string lastName;
-
-    public string FullName => $"{FirstName} {LastName}";
+    public PersonViewModel Person { get; set; } = new();
+    public PanelViewModel Panel { get; set; } = new();
 }
---
+++ Session1/PersonViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Session1;
+
+public partial class PersonViewModel : ObservableObject
+{
+    [ObservableProperty, NotifyPropertyChangedFor(nameof(FullName))]
+    string firstName;
+
+    partial void OnFirstNameChanged(string? oldValue, string newValue)
+    {
+
+    }
+
+    [ObservableProperty, NotifyPropertyChangedFor(nameof(FullName))]
+    string lastName;
+
+    public string FullName => $"{FirstName} {LastName}";
+}
