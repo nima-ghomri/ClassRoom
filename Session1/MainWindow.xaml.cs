f3a5b762ca7248f643ec337344b4ef0fd6b2691d
Nima <nima.ghomri@gmail.com>
2024-09-17 17:00:56 +0330

    Base view model

--- Session1/MainWindow.xaml.cs
+++ Session1/MainWindow.xaml.cs
@@ -1,14 +1,4 @@
-﻿using System.ComponentModel;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Session1;
 
@@ -19,38 +9,6 @@ public partial class MainWindow : Window
 {
     public MainWindow()
     {
-        DataContext = new MainWindowViewModel();
         InitializeComponent();
     }
-}
-
-public class MainWindowViewModel : INotifyPropertyChanged
-{
-    private string firstName;
-    private string lastName;
-
-    public string FirstName
-    {
-        get => firstName; set
-        {
-            firstName = value;
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(FirstName)));
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(FullName)));
-        }
-    }
-
-    public string LastName
-    {
-        get => lastName; set
-        {
-            lastName = value;
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(LastName)));
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(FullName)));
-        }
-    }
-
-    public string FullName => $"{FirstName} {LastName}";
-
-
-    public event PropertyChangedEventHandler? PropertyChanged;
 }
---
+++ Session1/MainWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Session1;
+
+public class MainWindowViewModel : BaseViewModel
+{
+    private string firstName;
+    private string lastName;
+
+    public string FirstName
+    {
+        get => firstName; set
+        {
+            firstName = value;
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(FullName));
+        }
+    }
+
+    public string LastName
+    {
+        get => lastName; set
+        {
+            lastName = value;
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(FullName));
+        }
+    }
+
+    public string FullName => $"{FirstName} {LastName}";
+}
+
+public abstract class BaseViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
