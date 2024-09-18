0a2b1a519cfb02c2f77b04e09d9af0ee971245dc
nima-ghomri <nima.ghomri@gmail.com>
2024-09-18 10:07:47 +0330

    Binding modes

--- Prepare1/MainWindowViewModel.cs
+++ Prepare1/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
 
 namespace Prepare1;
 
@@ -6,9 +7,17 @@ public partial class MainWindowViewModel : ObservableObject
 {
     [ObservableProperty, NotifyPropertyChangedFor(nameof(FullName))]
     string? firstName;
-    
+
     [ObservableProperty, NotifyPropertyChangedFor(nameof(FullName))]
     string? lastName;
 
     public string FullName => $"{FirstName} {LastName}";
+
+    public ObservableCollection<PersonViewModel> People { get; set; }
+}
+
+public partial class PersonViewModel : ObservableObject
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
 }
--- Session1/PanelViewModel.cs
+++ Session1/PanelViewModel.cs
@@ -9,5 +9,7 @@ namespace Session1
 {
     public partial class PanelViewModel : ObservableObject
     {
+        [ObservableProperty]
+        bool isOpen = true;
     }
 }
