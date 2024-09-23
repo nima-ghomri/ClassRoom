73422ce928fb3f74b2fc95c9ce87a6c5d1c9b9b2
nima-ghomri <nima.ghomri@gmail.com>
2024-09-23 08:28:16 +0330

    Custom control

---
+++ Session1/DialButton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Session1
+{
+    public class DialButton : Button
+    {
+        static DialButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(DialButton), new FrameworkPropertyMetadata(typeof(DialButton)));
+        }
+
+
+        public string Letters
+        {
+            get { return (string)GetValue(LettersProperty); }
+            set { SetValue(LettersProperty, value); }
+        }
+
+        public static readonly DependencyProperty LettersProperty =
+            DependencyProperty.Register("Letters", typeof(string), typeof(DialButton));
+    }
+}
